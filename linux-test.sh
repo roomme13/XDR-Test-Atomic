9d28122b2164410c8d3f2e44912a5101d8e7c9e0
roomme13 <roomme13@users.noreply.github.com>
2023-03-14 11:41:22 +0700

    add file test
    add file test

---
+++ linux-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+echo "Running Defense Evasion Attacks"
+./linux/defense-evasion/atomic-T1009.sh
+./linux/defense-evasion/atomic-T1027.sh
+
+echo "Running Discovery Attacks"
+./linux/discovery/atomic-T1016.sh
+./linux/discovery/atomic-T1033.sh
+./linux/discovery/atomic-T1082.sh
+
+echo "Running Impact Attacks"
+./linux/impact/atomic-T1496.sh
+
+echo "Running Persistence Attacks"
+./linux/persistence/atomic-T1156.sh
+./linux/persistence/atomic-T1158.sh
